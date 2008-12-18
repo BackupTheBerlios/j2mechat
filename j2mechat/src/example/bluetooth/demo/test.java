e18ee657b3b90ab4678f3b93e24aeed55141f8d0
jeremy13 <jeremy13>
2008-12-18 16:56:33 +0000

    no message

---
+++ j2mechat/src/example/bluetooth/demo/test.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package example.bluetooth.demo;
+
+/**
+ *
+ * @author jeremy
+ */
+public class test {
+
+}
